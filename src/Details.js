e0b668275f7008f7b67a84f19046750b486bb795
Kyle Adkins <kyle.m.adkins@gmail.com>
2021-12-31 00:29:03 -0600

    Create example error boundary component

--- src/Details.js
+++ src/Details.js
@@ -1,28 +1,30 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, petNotFound: false };
 
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
     const json = await res.json();
-    this.setState(
-      Object.assign(
-        {
-          loading: false,
-        },
-        json.pets[0]
-      )
-    );
+    if (!json.pets.length) {
+      this.setState({ loading: false, petNotFound: true });
+    } else {
+      this.setState(
+        Object.assign({ loading: false, hasError: false }, json.pets[0])
+      );
+    }
   }
 
   render() {
     if (this.state.loading) {
       return <h2>Loading...</h2>;
+    } else if (this.state.petNotFound) {
+      throw new Error("Pet not found");
     } else {
       const { name, animal, breed, city, state, description, images } =
         this.state;
@@ -39,4 +41,11 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+export default function DetailsWithErrorBoundary() {
+  const DetailsWithRouter = withRouter(Details);
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
